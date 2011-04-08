2766766f7dd1ebbc4b943ff86302fc7ff099b1df
Michael Deitcher <michaeldeitcher@yahoo.com>
2011-04-08 10:37:58 -0600

    updated buttons and cleanup

--- Resources/localwebcache/index.js
+++ Resources/localwebcache/index.js
@@ -1,6 +1,6 @@
 $().ready( function(){
 	$("div.remote_control ul li").click( function(){
-							  var loc = $(this).find("a").attr('href');
+		  var loc = $(this).addClass('pressed').find("a").attr('href');
 		  window.location = loc;
 	});
 });
