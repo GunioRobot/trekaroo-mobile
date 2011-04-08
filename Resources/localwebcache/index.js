a7ae9ef8f2bb40ee12da61d3c702b51b608f076c
Michael Deitcher <michaeldeitcher@yahoo.com>
2011-04-08 12:35:22 -0600

    css overlays for clicking vertical buttons

--- Resources/localwebcache/index.js
+++ Resources/localwebcache/index.js
@@ -1,6 +1,10 @@
 $().ready( function(){
+  $("div.remote_control ul.vertical li").each(function(i){
+    $(this).append('<div class="overlay"></div>');
+  });
+
 	$("div.remote_control ul li").click( function(){
-		  var loc = $(this).addClass('pressed').find("a").attr('href');
-		  window.location = loc;
+		var loc = $(this).addClass('pressed').find("a").attr('href');
+    window.location = loc;
 	});
 });
