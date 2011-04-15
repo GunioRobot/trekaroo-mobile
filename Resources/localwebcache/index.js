543355e1a5074aa230a05a02541e9da1bedfd6ae
Michael Deitcher <michaeldeitcher@yahoo.com>
2011-04-15 11:20:26 -0400

    Keep app from wigging out on multiple touch of home view.

--- Resources/localwebcache/index.js
+++ Resources/localwebcache/index.js
@@ -3,6 +3,7 @@ iosSendCommand = function( cmd, msg ){
 };
 
 $().ready( function(){
+  var clicked = false;
   $(window).bind("unload", function(){
     $("div.remote_control ul li").removeClass('pressed');
   });
@@ -12,11 +13,15 @@ $().ready( function(){
   });
 
 	$("div.remote_control ul li").click( function(){
+        if(clicked) return;
+        clicked = true;
 		var loc = $(this).addClass('pressed').find("a").attr('href');
 		window.location = loc;
 	});
 		  
 	$("li#non-mobile, li#feedback").click( function(e){
+        if(clicked) return;
+        clicked = true;
 		if( isIOSMobileApp() ){
 			e.preventDefault();
 			iosSendCommand("gotoExternalUrl", escape($(this).find('a').attr('href')));
